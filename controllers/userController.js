f2f320b66b9e5e63b9be7f4945e2e7064d015a87
Damma Deshan <dammamonnankulama@gmail.com>
2024-12-06 19:00:03 +0530

    Refactor user role validation functions for improved readability and conciseness👍

--- controllers/userController.js
+++ controllers/userController.js
@@ -72,28 +72,14 @@ export function loginUser (req,res){
     
 
 }
-export function isAdmin(req){
-    if(req.user==null){
-      return false
-    }
-  
-    if(req.user.type != "admin"){
-      return false
-    }
-  
-    return true
+export function isAdmin(req) {
+    return req.user?.type === "admin";
   }
   
- export function isCustomer(req){
-    if(req.user==null){
-      return false
-    }
   
-    if(req.user.type != "customer"){
-      return false
-    }
-  
-    return true
+  export function isCustomer(req) {
+    return req.user?.type === "customer";
   }
+  
    
   
